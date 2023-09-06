0d5832aeb4f4df0445278a876833b22a7b3baec4
Harry10459 <90323751+Harry10459@users.noreply.github.com>
2023-09-06 10:19:15 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="vfa",
-    version=0.1,
-    author="csuhan",
-    url="https://github.com/csuhan/VFA",
+    version=0.2,
+    author="Harry10459",
+    url="https://github.com/Harry10459/vfa_vqvae",
     description="Codebase for few-shot object detection",
     python_requires=">=3.6",
     packages=find_packages(exclude=('configs', 'data', 'work_dirs')),
