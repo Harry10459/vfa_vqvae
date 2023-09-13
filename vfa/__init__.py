3759f59bcc0f0cfa326170a84722c422ba3b3a5a
Harry10459 <90323751+Harry10459@users.noreply.github.com>
2023-09-13 14:05:41 +0800

    Update __init__.py

--- vfa/__init__.py
+++ vfa/__init__.py
@@ -1,5 +1,6 @@
 from .vfa_detector import VFA
 from .vfa_roi_head import VFARoIHead
 from .vfa_bbox_head import VFABBoxHead
+from .vqvae2 import VectorQuantizedVAE, to_scalar
 
-__all__ = ['VFA', 'VFARoIHead', 'VFABBoxHead']
+__all__ = ['VFA', 'VFARoIHead', 'VFABBoxHead', 'VectorQuantizedVAE', 'to_scalar']
